75da49f4f9eb33c002d2b7b71c449e526f7c4a96
NikMarcos <eronikita@yandex.ru>
2019-05-13 18:45:31 +0500

    remove has many article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,5 +2,4 @@ class Article < ApplicationRecord
   require 'carrierwave/orm/activerecord'
   mount_uploaders :pictures, ImagesUploader
   belongs_to :user, inverse_of: :articles
-  has_many :images, dependent: :destroy
 end
