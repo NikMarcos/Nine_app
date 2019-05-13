af2eb3a034671ffea7062edbb24aea3ae40826f7
NikMarcos <eronikita@yandex.ru>
2019-05-13 17:50:17 +0500

    has manu in article

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,5 @@ class Article < ApplicationRecord
   require 'carrierwave/orm/activerecord'
   mount_uploaders :pictures, ImagesUploader
   belongs_to :user, inverse_of: :articles
+  has_many :images, dependent: :destroy
 end
